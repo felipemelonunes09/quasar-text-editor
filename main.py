9f2a79dfb1519b6af70aecd69ee728f80b184083
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-09-02 21:30:28 -0300

    removed and improved file load

--- main.py
+++ main.py
@@ -6,33 +6,28 @@ import yaml
 from types import SimpleNamespace
 from core.file_handlers import FileLoader
 from shared import utils
-from screens.AttributeScreen import AttributeScreen
-from screens.EditorScreen import EditorScreen
+from core.screens.AttributeScreen import AttributeScreen
+from core.screens.EditorScreen import EditorScreen
 from core.file_objects import File
 
-
 class QuasarEditor:
-    def __init__(self, root, workspace = None):
-        
+    def __init__(self, root):
         
         self.__pallete        = None
+        self.root = root
         self.set_pallete(config.FS_PALLETES_DIR, config.FS_PALLETE_FILE, config.PALLETE)
         
-        # Set up the main window
         self.root = root
         self.root.title("QuasarEditor")
 
-        # Create the two frames with different background colors
         self.edit_screen        = EditorScreen(self.root, self)
         self.attribute_screen   = AttributeScreen(self.root, self)
         self.menu               = Menu.Menu(root, self)
-            
+        
+        self.__current_file: File 
+        
         self.edit_screen.show_default()
         self.menu.create()
-        
-        self.__loaded_content = None
-        self.__content_type   = None
-        self.__current_path   = None
         
         
     def set_pallete(self, dir_pallete: str, file_pallete: str, theme: str):
@@ -59,50 +54,41 @@ class QuasarEditor:
         
 
     def load_file(self, file: File = None):
-                
+               
+        self.set_current_file(file=file)
         file_loader = FileLoader(file=file)
-        self.set_loaded_content(file_loader.load())
-        self.set_content_type('FILE')
-        self.set_current_path(file.get_path())
         
-        self.edit_screen.show_editor(self.__loaded_content)        
+        self.edit_screen.show_editor(file_loader.load())        
             
     def load_dir(self):
         path = utils.open_dir()
-        if path:
-            self.set_loaded_content(path)
-            self.set_current_path(path)
-            self.set_content_type('DIR')
             
-            self.attribute_screen.show_dirtree(path)
-            self.edit_screen.show_editor("")
+        self.attribute_screen.show_dirtree(path)
+        self.edit_screen.show_editor("")
             
     def new_file(self):
-        self.set_content_type('FILE')
         self.edit_screen.set_visible()
         self.edit_screen.show_editor("")
+    
+    def get_current_file(self) -> File:
+        return self.__current_file
+    
+    def get_pallete(self):
+        return self.__pallete
         
     def get_content(self):
         return self.edit_screen.get_text()
     
-    def get_context(self):
-        return (self.__current_path, self.__content_type, self.__loaded_content)
+    def set_current_file(self, file: File):
+        self.__current_file = file
     
-    def set_current_path(self, path: str):
-        self.__current_path = path
-        
-    def set_loaded_content(self, content: str):
-        self.__loaded_content = content
-        
-    def set_content_type(self, type):
-        self.__content_type = type
-        
-    def get_pallete(self):
-        return self.__pallete
+
 
 # Create the main window and run the application
 if __name__ == "__main__":
     root = tk.Tk()
     app = QuasarEditor(root)
     root.mainloop()
+
+
 
